a47be43948e9a9bd6ffd5c57389ca7f849cc224f
niksan2007 <niksan333222@mail.ru>
2024-06-03 14:12:33 +0700

    Frontend правки 3

--- dbcs/simpleTest/transport/userRoute.js
+++ dbcs/simpleTest/transport/userRoute.js
@@ -20,21 +20,15 @@ lectorRoute = new r.Router();
 
 lectorRoute.get("/test",lecturerAuth, controllers.TestContoller.renderCreateTest);
 
-<<<<<<< HEAD
+
 lectorRoute.get('/constructor', lecturerAuth,controllers.TestContoller.renderClassicConstructor)
 
 
-// lectorRoute.put('/test/:id',lecturerAuth, controllers.TestContoller.updateTest)
 
-// lectorRoute.delete('/test/:id',lecturerAuth, controllers.TestContoller.deleteTest)
-
-// lectorRoute.get('/tests',lecturerAuth, controllers.TestContoller.getTests)
-
-=======
 lectorRoute.get('/constructor', lecturerAuth,controllers.TestContoller.renderClassicConstructor);
 
 lectorRoute.post('/test', controllers.TestContoller.createClassicTest);
->>>>>>> 3c829a699f244c405de1b97b0188075900d0e25a
+
 
 
 lectorRoute.put('/test/:id',lecturerAuth, controllers.TestContoller.updateTest)
